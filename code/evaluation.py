2bf13a93c6211abaf567b14008ffca2e1daea0f3
bartoszrud <bartoszrudnikowicz840@gmail.com>
2022-03-27 15:28:43 +0200

    fix

--- code/evaluation.py
+++ code/evaluation.py
@@ -1,6 +1,14 @@
 import numpy as np
 from pathlib import Path
-from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, roc_auc_score, auc, precision_recall_curve, roc_curve
+from sklearn.metrics import (
+    confusion_matrix,
+    accuracy_score,
+    f1_score,
+    roc_auc_score,
+    auc,
+    precision_recall_curve,
+    roc_curve,
+)
 import seaborn as sn
 import matplotlib.pyplot as plt
 
@@ -15,18 +23,17 @@ def write_results(model_name: str, metrics: dict, figures: dict) -> None:
         figures: Dictionary of matplotlib figures to be saved
     """
 
-    directory = f'code/results/{model_name}'
+    directory = f"code/results/{model_name}"
     Path(directory).mkdir(parents=True, exist_ok=True)
-    with open(f'{directory}/metrics.txt', 'w', encoding='utf-8') as f:
+    with open(f"{directory}/metrics.txt", "w", encoding="utf-8") as f:
         for key, value in metrics.items():
-            f.write(key + ': ' + str(value) + '\n')
+            f.write(key + ": " + str(value) + "\n")
         f.close()
     for name, fig in figures.items():
-        fig.savefig(f'{directory}/{name}', dpi=150)
+        fig.savefig(f"{directory}/{name}", dpi=150)
 
 
-def get_plot(x: list, y: list, xlabel: str, ylabel: str,
-             title: str) -> plt.figure:
+def get_plot(x: list, y: list, xlabel: str, ylabel: str, title: str) -> plt.figure:
     """Generates a matplotlib plot.
 
     Args:
@@ -39,7 +46,7 @@ def get_plot(x: list, y: list, xlabel: str, ylabel: str,
     Returns:
         A matplotlib figure with the plotted data.
     """
-    fig, ax = plt.subplots(facecolor='white')
+    fig, ax = plt.subplots(facecolor="white")
     ax.plot(x, y)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
@@ -47,8 +54,9 @@ def get_plot(x: list, y: list, xlabel: str, ylabel: str,
     return fig
 
 
-def evaluate_binary(y_true: np.ndarray, y_pred: np.ndarray,
-                    y_pred_probas: np.ndarray) -> (dict, dict):
+def evaluate_binary(
+    y_true: np.ndarray, y_pred: np.ndarray, y_pred_probas: np.ndarray
+) -> (dict, dict):
     """Computes metrics and figures for evaluating binary predictions
 
     Args:
@@ -63,33 +71,37 @@ def evaluate_binary(y_true: np.ndarray, y_pred: np.ndarray,
         values containing the data.
     """
 
-    f1 = f1_score(y_true, y_pred, average='binary')
+    f1 = f1_score(y_true, y_pred, average="binary")
     auroc = roc_auc_score(y_true, y_pred)
     precision, recall, _ = precision_recall_curve(y_true, y_pred_probas[:, 1])
-    prc = get_plot(recall, precision, 'Recall', 'Precision',
-                   'Precision-Recall Curve')
+    prc = get_plot(recall, precision, "Recall", "Precision", "Precision-Recall Curve")
     auprc = auc(recall, precision)
     fpr, tpr, _ = roc_curve(y_true, y_pred_probas[:, 1])
-    roc = get_plot(fpr, tpr, 'False Positive Rate', 'True Positive Rate',
-                   'Receiver Operating Characteristic')
+    roc = get_plot(
+        fpr,
+        tpr,
+        "False Positive Rate",
+        "True Positive Rate",
+        "Receiver Operating Characteristic",
+    )
     metrics = {
-        'F1 (binary)': f1,
-        'AUROC': auroc,
-        'AUPRC': auprc,
+        "F1 (binary)": f1,
+        "AUROC": auroc,
+        "AUPRC": auprc,
     }
 
     figures = {
-        'precision_recall': prc,
-        'roc': roc,
+        "precision_recall": prc,
+        "roc": roc,
     }
 
     return metrics, figures
 
 
 def evaluate_multiclass(y_true: np.ndarray, y_pred: np.ndarray):
-    f1 = f1_score(y_true, y_pred, average='macro')
+    f1 = f1_score(y_true, y_pred, average="macro")
     metrics = {
-        'F1 (macro)': f1,
+        "F1 (macro)": f1,
     }
     figures = {}
 
@@ -97,20 +109,19 @@ def evaluate_multiclass(y_true: np.ndarray, y_pred: np.ndarray):
 
 
 def get_heatmap(data):
-    fig, ax = plt.subplots(facecolor='white')
+    fig, ax = plt.subplots(facecolor="white")
 
-    cm_heatmap = sn.heatmap(data, annot=True, fmt='g', cmap='Greens', ax=ax)
+    cm_heatmap = sn.heatmap(data, annot=True, fmt="g", cmap="Greens", ax=ax)
     cm_heatmap.set(
-        xlabel='Predicted class',
-        ylabel='True class',
-        title='Confusion matrix',
+        xlabel="Predicted class", ylabel="True class", title="Confusion matrix",
     )
 
     return fig
 
 
-def evaluate(model_name: str, y_pred_probas: np.ndarray, y_true: np.ndarray,
-             save_results: bool) -> None:
+def evaluate(
+    model_name: str, y_pred_probas: np.ndarray, y_true: np.ndarray, save_results: bool
+) -> None:
     """Computes evaluation metrics for given predictions.
 
     Args:
@@ -131,7 +142,7 @@ def evaluate(model_name: str, y_pred_probas: np.ndarray, y_true: np.ndarray,
     # Probabilities -> class predictions
     y_pred = np.argmax(y_pred_probas, axis=1)
 
-    cm_norm = confusion_matrix(y_true, y_pred, normalize='true')
+    cm_norm = confusion_matrix(y_true, y_pred, normalize="true")
     cm_norm = np.around(cm_norm, 3)
     cm = confusion_matrix(y_true, y_pred)
 
@@ -140,14 +151,14 @@ def evaluate(model_name: str, y_pred_probas: np.ndarray, y_true: np.ndarray,
 
     acc = accuracy_score(y_true, y_pred)
 
-    if n_classes == 2:
+    if n_classes == 2 or n_classes == 1:
         metrics, figures = evaluate_binary(y_true, y_pred, y_pred_probas)
     else:
         metrics, figures = evaluate_multiclass(y_true, y_pred)
 
-    metrics['Accuracy'] = acc
-    figures['confusion_matrix'] = cm_heatmap
-    figures['confusion_matrix_norm'] = cm_heatmap_norm
+    metrics["Accuracy"] = acc
+    figures["confusion_matrix"] = cm_heatmap
+    figures["confusion_matrix_norm"] = cm_heatmap_norm
 
     if save_results:
         write_results(model_name, metrics, figures)
@@ -156,17 +167,17 @@ def evaluate(model_name: str, y_pred_probas: np.ndarray, y_true: np.ndarray,
 
 
 # Example use
-if __name__ == '__main__':
+if __name__ == "__main__":
     from mock.sample_predictions import MULTICLASS_TEST_PREDS, BINARY_TEST_PREDS
 
-    y_pred_probas = MULTICLASS_TEST_PREDS['y_pred_probas']
-    y_true = MULTICLASS_TEST_PREDS['y_true']
+    y_pred_probas = MULTICLASS_TEST_PREDS["y_pred_probas"]
+    y_true = MULTICLASS_TEST_PREDS["y_true"]
 
-    evaluate('test_multiclass', y_pred_probas, y_true, save_results=True)
+    evaluate("test_multiclass", y_pred_probas, y_true, save_results=True)
 
     plt.clf()
 
-    y_pred_probas = BINARY_TEST_PREDS['y_pred_probas']
-    y_true = BINARY_TEST_PREDS['y_true']
+    y_pred_probas = BINARY_TEST_PREDS["y_pred_probas"]
+    y_true = BINARY_TEST_PREDS["y_true"]
 
-    evaluate('test_binary', y_pred_probas, y_true, save_results=True)
+    evaluate("test_binary", y_pred_probas, y_true, save_results=True)
