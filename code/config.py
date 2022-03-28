918f6616c6b96e26a5e34dc3925d05f632d80d0a
Frithiof Ekström <frithiof.ekstrom@gmail.com>
2022-03-28 17:17:07 +0200

    Update default config values

--- code/config.py
+++ code/config.py
@@ -1,4 +1,9 @@
-# Switch to 0 if GPU is not available
-USE_GPU = 0
-RETRAIN_MODELS = True
-DRY_RUN = True
+# -1 if all GPUs should be used, 0 if no GPUs should be used
+USE_GPU = -1
+
+# True if models should be retrained from scratch, False if they
+# should be loaded from file
+RETRAIN_MODELS = False
+
+# True if results should not be saved, False otherwise
+DRY_RUN = False
